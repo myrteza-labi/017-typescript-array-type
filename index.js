276684a74318ab78212263556a75e306392cb371
myrteza <myrteza@hotmail.fr>
2023-10-07 18:18:52 +0200

    project done

--- index.js
+++ index.js
@@ -3,4 +3,8 @@ array[3] = "4";
 array.push("5");
 var scores = [];
 scores = ["Programming", 5, "Software design", 4];
+//should use this 
+var number = [];
+//instead of this
+var number2 = [];
 console.log(scores);
--- index.ts
+++ index.ts
@@ -8,5 +8,10 @@ let scores : (string|number)[] = [];
 
 scores = ["Programming", 5, "Software design", 4]; 
 
+//should use this 
+let number = []; 
+
+//instead of this
+let number2 : number[] = []; 
 
 console.log(scores); 
